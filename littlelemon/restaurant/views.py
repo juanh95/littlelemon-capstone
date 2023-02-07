82923cfc2a219ea3579b38ece2486b294568638d
Juan Hernandez <hernandezjuanm39@gmail.com>
2023-02-06 18:54:39 -0600

    added token authentication

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -9,15 +9,18 @@ def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-
-    permission_classes = [IsAuthenticated]
