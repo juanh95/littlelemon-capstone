34ae4353c17697f2ec9e072168c124871d29769a
Juan Hernandez <hernandezjuanm39@gmail.com>
2023-02-08 19:21:12 -0600

    updated tests

--- littlelemon/tests/test_models.py
+++ littlelemon/tests/test_models.py
@@ -1,7 +1,12 @@
 from django.test import TestCase
-from restaurant.models import Menu
+from restaurant.models import Menu, Booking
 
 class MenuItemTest(TestCase): 
     def test_get_item(self):
         item = Menu.objects.create(title="IceCream", price=80, inventory=10)
         self.assertEqual(f'{item.title}: {str(item.price)}', "IceCream: 80")
+
+class BookingTest(TestCase):
+    def test_get_booking(self):
+        booking1 = Booking.objects.create(name="BDay Dinner", no_of_guests=3, bookingdate="2023-02-07T14:38:19Z")
+        self.assertEqual(f'{booking1.bookingdate}: {booking1.name}', "2023-02-07T14:38:19Z: BDay Dinner")
--- littlelemon/tests/test_views.py
+++ littlelemon/tests/test_views.py
@@ -3,30 +3,48 @@ from restaurant.models import Menu
 from restaurant.serializers import MenuSerializer
 from rest_framework.test import APIClient
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class MenuViewTest(TestCase):   
     def setup(self):
         item1 = Menu.objects.create(title="Peanuts", price=1.99, inventory=2)
         item2 = Menu.objects.create(title="Cake", price=5.99, inventory=3)
         items = Menu.objects.all()
-
-        return MenuSerializer(items, many=True)
+        
+        return items
+    
+    def setup_single(self):
+        item = Menu.objects.create(title="Peanuts", price=1.99, inventory=2)
+        return item
 
     def test_getall_unauth(self):
         client = APIClient()
-        response = client.get('/restaurant/menu/')
+        response = client.get(reverse('MenuItemsView'))
         self.assertEquals(response.status_code, 401)
     
     def test_getall_auth(self):
         client = APIClient()
         client.force_login(User.objects.get_or_create(username='testuser')[0]) 
         items = self.setup()
-        response = client.get('/restaurant/menu/')
+        response = client.get(reverse('MenuItemsView'))
         self.assertEquals(response.status_code, 200)
         self.assertQuerysetEqual(
             response.data,
-            items.data
+            MenuSerializer(items, many=True).data
         )
         client.logout()
+    
+    def test_getsingle_auth(self):
+        client = APIClient()
+        client.force_login(User.objects.get_or_create(username='testuser')[0]) 
+        item = self.setup_single()
+        response = client.get(reverse('SingleMenuItemView', args=[item.pk]))
+        self.assertEquals(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.data,
+            MenuSerializer(item).data
+        )
+        client.logout()
+        
 
     
